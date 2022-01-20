d51d375f4cd5f292737f520fd8046a69b2db15a9
balazskosa <ot4wi3@inf.elte.hu>
2022-01-20 02:10:36 +0100

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Notebook';
 
-  open (even: any) {
-    console.log("opened");
+  constructor(private route:Router) {
   }
 
-  close(event: any) {
-    console.log("closed");
+  async logout() {
+    await this.route.navigate(['/login']);
   }
-
 }
 
 
